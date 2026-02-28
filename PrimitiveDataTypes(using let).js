4bf116527ab37b1a7c5dea737e6a17c382793f5e
sangirthi <sangirthiyayan.c@testleaf.com>
2026-02-28 17:49:31 +0530

    Add files via upload

---
+++ PrimitiveDataTypes(using let).js
@@ -0,0 +1,11 @@
+let firstName = "Saranya";
+let companyName = "Testleaf";
+let mobileNumber = 9876543210;
+let isAutomation = true;
+let hasPlaywright; // not assigned
+
+console.log(firstName, typeof firstName);
+console.log(companyName, typeof companyName);
+console.log(mobileNumber, typeof mobileNumber);
+console.log(isAutomation, typeof isAutomation);
+console.log(hasPlaywright, typeof hasPlaywright);
